a6f56e7988f785bf1352c7693fa96108f37b520f
Raymond Morris <raymondmorris80@gmail.com>
2021-11-09 11:37:43 -0700

    Backend is done

---
+++ server.js
@@ -0,0 +1,49 @@
+//////////////////////////
+// Import Dependencies
+//////////////////////////
+const express = require("express")
+const cors = require("cors")
+
+///////////////////////
+// Import Json
+///////////////////////
+const project = require("./projects.json")
+const about = require("./about.json")
+
+/////////////////////////
+// App 
+/////////////////////////
+const app = express()
+
+/////////////////////////
+// Middleware
+/////////////////////////
+app.use(cors())
+
+/////////////
+// Routes
+////////////
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+// Projects Route
+app.get("/projects", (req, res) => {
+    res.json(projects)
+})
+
+// About Route
+app.get("/about", (req, res) => {
+    res.json(about)
+})
+
+
+
+/////////////////////////
+// Server Listener
+/////////////////////////
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+})
